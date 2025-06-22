c4696df394cacc1b5897f9352c80048dc4541304
Admare <admaremhamad@gmail.com>
2025-06-22 18:43:29 +0300

    Add initial configuration files for server setup

---
+++ src/serverConfig.rs
@@ -0,0 +1,16 @@
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct ServerConfig {
+    pub server_name: String,
+    pub server_address: Vec<(String, u16)>, //ip and Port
+    pub max_body_size: usize,               // in bytes
+    pub router: Vec<RouterConfig>,
+    pub error_page: HashMap<u16, String>, // status code and page path
+}
+#[derive(Debug, PartialEq, Clone, Copy)]
+struct RouterConfig {
+    pub path: String,
+    pub methods: Vec<String>, // GET, POST, etc.
+    pub root: String,
+    pub index: Option<String>, // default file to serve
+    pub cgi: Option<(String, String)>,
+}
