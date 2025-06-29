13e804fc6fc9b117a2c9ead212ae6b4f1a03351e
mhadmare <admaremhamad@gmail.com>
2025-06-29 10:45:41 +0300

    Fix typo in import statement for RawFd in main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use serverConfig::ServerConfig;
 use std::collections::HashMap;
 
 use libc;
-use std::os::unix::io::{AsRawFd, RawFd}; // Not available on Windows
+use std::os::unix::io::{AsRawFd, Ra`wFd}; // Not available on Windows
 
 fn main() {
     let servers = json_parser();
