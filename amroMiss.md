5c9ec959435bd4d35430f2399b2c7789ed9cc113
Admare <admaremhamad@gmail.com>
2025-06-29 15:55:07 +0300

    deleted amro

--- amroMiss.md
+++
@@ -1,9 +0,0 @@
-Member 4: 
-1- Static File Serving + Uploads
-2-Serve static files based on request path.
-
-3-Return 404, 403 errors if file doesn't exist.
-
-4-Implement file upload via POST and enforce max body size.
-
-🧠 Skills: File I/O, path resolution, security checks.
--- src/main.rs
+++ src/main.rs
@@ -9,40 +9,12 @@ use serde::Deserialize;
 use serverConfig::ServerConfig;
 use std::collections::HashMap;
 
-mod static_file;
 use static_file::{FileResponse, build_http_response, read_static_file};
-mod upload_handler;
-use upload_handler::{build_upload_response, handle_file_upload};
 
 use libc;
 use std::os::unix::io::{AsRawFd, RawFd}; // Not available on Windows
 
 fn main() {
-    // مثال يدوي: كأنك استقبلت طلب من المتصفح
-
-    // 1. طلب GET لملف ثابت
-    let method = "GET";
-    let path = "/index.html";
-
-    if method == "GET" {
-        let result = read_static_file(path);
-        let response = build_http_response(result);
-        println!("--- GET Response ---");
-        println!("{}", String::from_utf8_lossy(&response));
-    }
-
-    // 2. طلب POST لرفع ملف
-    let method = "POST";
-    let content_type = "multipart/form-data; boundary=----XYZ";
-    let fake_body = fs::read("tests/upload_example_body.txt").unwrap_or_default();
-
-    if method == "POST" {
-        let result = handle_file_upload(&fake_body, content_type);
-        let response = build_upload_response(result);
-        println!("--- POST Response ---");
-        println!("{}", String::from_utf8_lossy(&response));
-    }
-
     let servers = json_parser();
     for server in &servers {
         listener_socket(&server);
